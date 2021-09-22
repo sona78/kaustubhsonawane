8b1d36cce0bfa63b16449d9166d695c5c131975e
Kaustubh Sonawane <sona.om78@gmail.com>
2021-09-21 21:21:09 -0500

    fix image

--- src/seo.js
+++ src/seo.js
@@ -8,7 +8,7 @@ const SEO = ({ title, description, keywords, image}) => {
     <meta name="keywords" content="Kaustubh Sonawane, entrepreneur, computer science, student"/>
     <meta name="title" property="og:title" content="Kaustubh Sonawane is a driven student with a passion for computer science and entrepreneurship"/>
     <meta property="og:type" content="Website"/>
-    <meta name="image" property="og:image" content="https://imgur.com/YxUTpFZ"/>
+    <meta name="image" property="og:image" content="https://challengepost-s3-challengepost.netdna-ssl.com/photos/production/user_photos/001/410/949/datas/profile.png"/>
     <meta name="author" content="Kaustubh Sonawane"/>
   </Helmet>
    );
