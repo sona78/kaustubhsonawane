cf8ab51486417eda3b1e5bb4a012c7f32507ddfb
Kaustubh Sonawane <sona.om78@gmail.com>
2021-09-21 21:18:35 -0500

    SEO fix

--- src/seo.js
+++ src/seo.js
@@ -3,21 +3,14 @@ import React from 'react';
 
 const SEO = ({ title, description, keywords, image}) => {
       return (
-  <Helmet htmlAttributes={{ lang: 'en' }} title={title} meta={[
-          {
-            name: 'description',
-            content: description,
-          },
-          {
-            name: 'keywords',
-            content: keywords.join(),
-          },
-          {
-            name: 'image',
-            content: image,
-          }
-          ]}
-      />
+  <Helmet htmlAttributes={{ lang: 'en' }}>
+    <meta name="description" property="og:description" content="Personal Website for Kaustubh Sonawane"/>
+    <meta name="keywords" content="Kaustubh Sonawane, entrepreneur, computer science, student"/>
+    <meta name="title" property="og:title" content="Kaustubh Sonawane is a driven student with a passion for computer science and entrepreneurship"/>
+    <meta property="og:type" content="Website"/>
+    <meta name="image" property="og:image" content="https://imgur.com/YxUTpFZ"/>
+    <meta name="author" content="Kaustubh Sonawane"/>
+  </Helmet>
    );
   }
   export default SEO;
