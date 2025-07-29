0d40a9544554775a953a5467d3af26f4dbebeadd
sona78 <sona.om78@gmail.com>
2025-07-29 09:06:22 -0700

    Added Projects

--- src/sections/projects.js
+++ src/sections/projects.js
@@ -6,21 +6,55 @@ import { Card, CardTitle, CardBody } from "shards-react";
 
 const projects = [
   {
+    title: "Clarity",
+    description:
+      "AI-generated career plans: predict the future of an industry and create actionable steps for upskilling",
+    stack: "Flask, Prompt Engineering, Exa, React, Supabase",
+    link: "https://github.com/sona78/Clarity",
+  },
+  {
+    title: "Storybook",
+    description:
+      "Public MCP Server that allows users to collaborate on AI stories published to social media",
+    stack: "MCP, Tool Calling, AI Agents, Next.js, Flask",
+    link: "https://github.com/sona78/Storybook",
+  },
+  {
+    title: "Me.md",
+    description:
+      "A personalized browser agent that completes actions based on discovered insights about you",
+    stack: "Chrome Extension, BrowserUse, Playwright, Dynamic Web Scraping",
+    link: "https://github.com/masterfung/agihousehackathon",
+  },
+  {
+    title: "Citation Crawler",
+    description: "Write an article, then automatically find citations that support your claims",
+    stack: "Exa, Next.js, Python, Flask, GPT-3.5",
+    link: "https://github.com/sona78/Citation-Crawler",
+  },
+  {
     title: "Apollo",
     description: "A self completing to-do list using Generative AI Agents",
-    stack: "Next.js, Flask, Python, AWS, GPT-4",
-    link: "https://drive.google.com/file/d/1Z0olu0kkQKZ9CuU8sgcVbCKg6K6VCD9J/view?usp=sharing",
+    stack: "Next.js, Flask, Python, AWS, Function Calling, AI Agents",
+    link: "https://github.com/sanjitjuneja/agent_todo_list",
+  },
+  {
+    title: "Bee Cave Foundation: Art Gallery",
+    description:
+      "Art gallery website for an Austin-area non-profit that supports community art initatives",
+    stack: "React, Google API, Firebase",
+    link: "https://github.com/tpeo/BeecaveFoundation",
   },
   {
     title: "Scribe",
     description: "AI-powered collective notetaking platform",
-    stack: "Node.js, Python, AWS Lambda, Ada-002 Embeddings LLM",
+    stack: "Pinecone, Node.js, Python, AWS Lambda, Ada-002 Embeddings LLM",
     link: "https://scribenotes.netlify.app/",
   },
   {
     title: "LungStat",
     description: "Physician-assistant ML detection of Lung Cancer Tumors",
-    stack: "Python, TensorFlow, React, OpenCV",
+    stack: "TensorFlow, Python, React, OpenCV",
     link: "https://drive.google.com/file/d/1Z0olu0kkQKZ9CuU8sgcVbCKg6K6VCD9J/view?usp=sharing",
   },
 
